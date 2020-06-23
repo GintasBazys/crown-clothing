7b3ddb2d27d434d3f4e022a4aca061038ec773c5
Gintas Bazys <bazys.gintas@gmail.com>
2020-06-23 18:31:49 +0300

    homepage, directory, menuitem components

--- crown-clothing/src/App.js
+++ crown-clothing/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import HomePage from "./HomepageComponent";
+import HomePage from "./pages/homepage/HomepageComponent";
 
 function App() {
   return (
