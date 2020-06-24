c065c91d2fa28f071f25c620e4afc5eb744ba994
Gintas Bazys <bazys.gintas@gmail.com>
2020-06-24 11:07:59 +0300

    React routing

--- crown-clothing/src/App.js
+++ crown-clothing/src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from "react-router-dom"
 
 import HomePage from "./pages/homepage/HomepageComponent";
+
+const HatsPage = () => {
+    return (
+            <div>
+                <h1>HATS PAGE</h1>
+            </div>
+        )
+
+}
 
 function App() {
   return (
     <div>
-      <HomePage />
+        <Switch>
+            <Route exact={true} path="/" component={HomePage} />
+            <Route path="/hats" component={HatsPage} />
+        </Switch>
     </div>
   );
 }
