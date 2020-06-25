059d6ce520f02bc12b89a88928e03fdc9d694b2b
Gintas Bazys <bazys.gintas@gmail.com>
2020-06-25 09:56:37 +0300

    Storing user data in firebase

--- crown-clothing/src/App.js
+++ crown-clothing/src/App.js
@@ -6,7 +6,7 @@ import HomePage from "./pages/homepage/Homepage";
 import ShopPage from "./pages/shop/ShopPage";
 import Header from "./components/header/HeaderComponent";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/SignInAndSignOut";
-import { auth } from "./firebase/firebaseUtils";
+import { auth, createUserProfileDocument } from "./firebase/firebaseUtils";
 
 class App extends React.Component {
     constructor(props) {
@@ -20,10 +20,21 @@ class App extends React.Component {
     unsubscribeFromAuth = null;
 
     componentDidMount() {
-        this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-            this.setState({currentUser: user});
-            console.log(user);
-        })
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+            if(userAuth) {
+                const userRef = await createUserProfileDocument(userAuth);
+
+                userRef.onSnapshot(snapShot => {
+                    this.setState({
+                        currentUser: {
+                            id: snapShot.id,
+                            ...snapShot.data()
+                        }
+                    });
+                });
+            }
+            this.setState({currentUser: userAuth})
+        });
     }
 
     componentWillUnmount() {
