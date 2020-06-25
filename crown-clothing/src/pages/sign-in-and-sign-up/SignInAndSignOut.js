d5361a3acb4fa1877c166bb698f33632df26f926
Gintas Bazys <bazys.gintas@gmail.com>
2020-06-25 10:21:15 +0300

    Sign up with email and password

--- crown-clothing/src/pages/sign-in-and-sign-up/SignInAndSignOut.js
+++ crown-clothing/src/pages/sign-in-and-sign-up/SignInAndSignOut.js
@@ -3,10 +3,14 @@ import React from "react";
 import "./sign-in-and-sign-out-styles.scss";
 
 import SignIn from "../../components/sign-in/SignInComponent";
+import SignUp from "../../components/sign-up/SignUpComponent";
 
 const SignInAndSignUpPage = () => {
     return (
-        <div className="sign-in-and-sign-up"><SignIn /></div>
+        <div className="sign-in-and-sign-up">
+            <SignIn />
+            <SignUp />
+        </div>
     )
 }
 
