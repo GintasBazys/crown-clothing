9987e64f43663ec858e6b8fe00dfa5380f62a75e
Gintas Bazys <bazys.gintas@gmail.com>
2020-06-26 15:30:37 +0300

    userReducer and userActions to header and app component

--- crown-clothing/src/App.js
+++ crown-clothing/src/App.js
@@ -7,15 +7,10 @@ import ShopPage from "./pages/shop/ShopPage";
 import Header from "./components/header/HeaderComponent";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/SignInAndSignOut";
 import { auth, createUserProfileDocument } from "./firebase/firebaseUtils";
+import { connect } from "react-redux";
+import { setCurrentUser } from "./redux/user/UserActions";
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            currentUser: null
-        }
-    }
 
     unsubscribeFromAuth = null;
 
@@ -25,15 +20,13 @@ class App extends React.Component {
                 const userRef = await createUserProfileDocument(userAuth);
 
                 userRef.onSnapshot(snapShot => {
-                    this.setState({
-                        currentUser: {
+                    this.props.setCurrentUser({
                             id: snapShot.id,
                             ...snapShot.data()
-                        }
                     });
                 });
             }
-            this.setState({currentUser: userAuth})
+            this.props.setCurrentUser(userAuth)
         });
     }
 
@@ -44,7 +37,7 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <Header currentUser={this.state.currentUser} />
+                <Header />
                 <Switch>
                     <Route exact={true} path="/" component={HomePage} />
                     <Route path="/shop" component={ShopPage} />
@@ -55,4 +48,8 @@ class App extends React.Component {
     }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+    setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(null, mapDispatchToProps)(App);
