a7897fc1fa10592d066fdcb5b1ddc249f1b78939
Gintas Bazys <bazys.gintas@gmail.com>
2020-06-27 11:47:50 +0300

    Cart item component

--- crown-clothing/src/components/cart-dropdown/CartDropdownComponent.js
+++ crown-clothing/src/components/cart-dropdown/CartDropdownComponent.js
@@ -1,15 +1,24 @@
 import React from "react";
 
 import CustomButton from "../custom-button/CustomButtonComponent";
+import CartItem from "../cart-item/CartItemComponent";
+import { connect } from "react-redux";
 import "./cart-dropdown-styles.scss";
 
-const CartDropdown = () => {
+const CartDropdown = ({cartItems}) => {
     return (
         <div className="cart-dropdown">
-            <div className="cart-items"/>
+            <div className="cart-items">
+                {
+                    cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem} />)
+                }
+            </div>
             <CustomButton>GO TO CHECKOUT</CustomButton>
         </div>
      )
 }
+const mapStateToProps = ({cart: {cartItems}}) => ({
+    cartItems
+})
 
-export default CartDropdown;
+export default connect(mapStateToProps)(CartDropdown);
