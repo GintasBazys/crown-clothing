c2e875d10e6682544ff9b3f34adc5a9d1c9ea326
Gintas Bazys <bazys.gintas@gmail.com>
2020-07-01 09:20:28 +0300

    Shop data to reducer

--- crown-clothing/src/pages/shop/ShopPage.js
+++ crown-clothing/src/pages/shop/ShopPage.js
@@ -1,16 +1,40 @@
 import React from "react";
 import { Route } from "react-router-dom";
+import { connect } from "react-redux";
 
 import CollectionsOverview from "../../components/collection-overview/CollectionOverviewComponent";
 import CollectionPage from "../collection/Collection";
 
-const ShopPage = ({match}) => {
+import { firestore, convertCollectionsSnapshotToMap } from "../../firebase/firebaseUtils";
+import { updateCollections } from "../../redux/shop/ShopActions";
+
+class ShopPage extends React.Component{
+    unsubscribeFromSnapshot = null;
+
+    componentDidMount() {
+        const { updateCollections } = this.props;
+        const collectionRef = firestore.collection("collections");
+
+        collectionRef.onSnapshot(async snapshot => {
+            const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+            updateCollections(collectionsMap);
+        });
+    }
+
+    render(){
+        const {match} = this.props;
         return (
             <div className="shop-page">
                 <Route exact path={`${match.path}`} component={CollectionsOverview} />
                 <Route path={`${match.path}/:collectionId`} component={CollectionPage}/>
             </div>
         )
+    }
+
 }
 
-export default (ShopPage);
+const mapDispatchToProps = dispatch => ({
+    updateCollections: collectionsMap => dispatch(updateCollections(collectionsMap))
+});
+
+export default connect(null, mapDispatchToProps)(ShopPage);
